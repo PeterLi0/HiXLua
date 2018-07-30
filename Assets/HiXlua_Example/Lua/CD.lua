87ca5e36004093514371b147cc1850346766aa52
Hiram <hiramtan@live.com>
2018-07-30 14:36:47 +0800

    add cd logic

--- Assets/HiXlua_Example/Lua/CD.lua
+++ Assets/HiXlua_Example/Lua/CD.lua
@@ -1,11 +1,3 @@
-local image
-
-image = CS.UnityEngine.GameObject.Find("Image").gameObject:GetComponent(typeof(CS.UnityEngine.UI.Image))
-print(image.name)
-
-image.type = CS.UnityEngine.UI.Image.Type.Filled
-image.fillMethod = CS.UnityEngine.UI.Image.FillMethod.Radial360
-
 
 local util = require("xlua.util")
 local runnerGo = CS.UnityEngine.GameObject("Coroutine_Runner")
@@ -16,29 +8,40 @@ local function async_yield_return(to_yield,cb)
     runner:YieldAndCallback(to_yield,cb)
 end
 
+
 local yield_return = util.async_to_sync(async_yield_return)
 
-print("cd start")
 
-local co = coroutine.create(function()
+local image
 
-while(image.fillAmount>0)
-do
-    image.fillAmount=image.fillAmount-0.01
-    yield_return(CS.UnityEngine.WaitForSeconds(0.01))
+local button
+
+image = CS.UnityEngine.GameObject.Find("Image").gameObject:GetComponent(typeof(CS.UnityEngine.UI.Image))
+image.type = CS.UnityEngine.UI.Image.Type.Filled
+image.fillMethod = CS.UnityEngine.UI.Image.FillMethod.Radial360
+
+button = CS.UnityEngine.GameObject.Find("Button").gameObject:GetComponent(typeof(CS.UnityEngine.UI.Button))
+
+
+local isCDing = false
+
+function CD()
+    if(isCDing==false)then
+        isCDing = true
+        print("cd start")
+
+        local co = coroutine.create(function()
+    
+        while(image.fillAmount>0)
+        do
+            image.fillAmount=image.fillAmount-0.01
+         yield_return(CS.UnityEngine.WaitForSeconds(0.01))
+        end
+        print("cs finish")
+        isCDing = false
+        image.fillAmount=1   
+        end)    
+    coroutine.resume(co)
+    end
 end
-
-
---[[
-print("start")
-local s = os.time()
-yield_return(CS.UnityEngine.WaitForSeconds(3))
-print("wait")
-]]
-
-
-end)
-
-coroutine.resume(co)
-
-print("cs finish")
+button.onClick:AddListener(CD)
